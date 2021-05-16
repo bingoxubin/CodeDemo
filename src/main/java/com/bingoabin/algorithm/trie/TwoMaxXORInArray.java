32a4c960292ada3b84233066e33ed3cf44a0d09c
xubin03 <xubin03@baijia.com>
2021-05-17 01:54:30 +0800

    TwoMaxXORInArray

---
+++ src/main/java/com/bingoabin/algorithm/trie/TwoMaxXORInArray.java
@@ -0,0 +1,64 @@
+package com.bingoabin.algorithm.trie;
+
+/**
+ * @author xubin03
+ * @date 2021/5/17 1:34 上午
+ */
+public class TwoMaxXORInArray {
+	//Leetcode.421 数组中两个数的最大异或值  https://leetcode-cn.com/problems/maximum-xor-of-two-numbers-in-an-array/
+	//样例：[3,10,5,25,2,8]  返回 5 XOR 25 = 28
+	//分析：给一个数组，随机选取两个数，找出可以求得的最大的异或值
+	//思路：用前缀树，TrieTree，先将数组中的数据按照二进制从首位到末尾插入到前缀树中（0，1)树，再遍历原数组，遍历一个，从TrieTree树中找到对应的最大值，记录最大值结果比较
+	public static void main(String[] args) {
+		int[] arr = {3, 10, 5, 25, 2, 8};
+		TwoMaxXORInArray twoMaxXORInArray = new TwoMaxXORInArray();
+		System.out.println(twoMaxXORInArray.findMaximumXOR(arr));
+	}
+
+	//最终方法，求出数组中，两个数的最大异或和
+	public int findMaximumXOR(int[] nums) {
+		int max = 0;
+		for (int num : nums) {
+			add(num);
+			int value = getVal(num);
+			max = Math.max(max, num ^ value);
+		}
+		return max;
+	}
+
+	//初始化根节点
+	TrieTree root = new TrieTree();
+
+	//将一个数据添加到TrieTree树中
+	public void add(int num) {
+		TrieTree p = root;
+		for (int i = 31; i >= 0; i--) {
+			int temp = (num >> i) & 1;
+			if (p.node[temp] == null) p.node[temp] = new TrieTree();
+			p = p.node[temp];
+		}
+	}
+
+	//从TrieTree树中，找出与num求得最大异或和的数
+	public int getVal(int num) {
+		TrieTree p = root;
+		int res = 0;
+		for (int i = 31; i >= 0; i--) {
+			int a = (num >> i) & 1;
+			int b = 1 - a;
+			//找跟a相反的在TrieTree中有没有，没有的话，继续遍历a,有的话从b开始找
+			if (p.node[b] == null) {
+				res |= a << i;
+				p = p.node[a];
+			} else {
+				res |= b << i;
+				p = p.node[b];
+			}
+		}
+		return res;
+	}
+
+	class TrieTree {
+		TrieTree[] node = new TrieTree[2];
+	}
+}
