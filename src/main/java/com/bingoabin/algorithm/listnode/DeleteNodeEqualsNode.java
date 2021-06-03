986402a21313cd5e7824afebe039b1bba8571232
xubin03 <xubin03@baijia.com>
2021-06-04 00:25:34 +0800

    listnode

--- src/main/java/com/bingoabin/algorithm/listnode/DeleteNodeEqualsNode.java
+++ src/main/java/com/bingoabin/algorithm/listnode/DeleteNodeEqualsNode.java
@@ -5,5 +5,41 @@ package com.bingoabin.algorithm.listnode;
  * @date 2021/6/3 12:44 下午
  */
 public class DeleteNodeEqualsNode {
+	//Leetcode 203. 移除链表元素
+	//样例：输入：head = [1,2,6,3,4,5,6], val = 6 输出：[1,2,3,4,5]
+	//分析：给一个链表 删除其中为指定元素的值
+	//思路：一个节点pre指向亚节点，一个cur指向head，当cur.val = val,那么pre.next = cur.next;如果不相等，那么pre = cur,意思就是向后移，最后判断完就cur = cur.next;
+	public static void main(String[] args) {
+		ListNode node1 = new ListNode(1);
+		ListNode node2 = new ListNode(1);
+		ListNode node3 = new ListNode(2);
+		ListNode node4 = new ListNode(3);
+		ListNode node5 = new ListNode(3);
+		node1.next = node2;
+		node2.next = node3;
+		node3.next = node4;
+		node4.next = node5;
 
+		ListNode res = removeElements(node1, 1);
+		while (res != null) {
+			System.out.print(res.val + " ");
+			res = res.next;
+		}
+	}
+
+	public static ListNode removeElements(ListNode head, int val) {
+		ListNode dummy = new ListNode(-1);
+		dummy.next = head;
+		ListNode pre = dummy;
+		ListNode cur = head;
+		while (cur != null) {
+			if (cur.val == val) {
+				pre.next = cur.next;
+			} else {
+				pre = cur;
+			}
+			cur = cur.next;
+		}
+		return dummy.next;
+	}
 }
--- src/main/java/com/bingoabin/algorithm/listnode/DeleteRepetitionNodeAll.java
+++ src/main/java/com/bingoabin/algorithm/listnode/DeleteRepetitionNodeAll.java
@@ -5,4 +5,45 @@ package com.bingoabin.algorithm.listnode;
  * @date 2021/6/3 12:44 下午
  */
 public class DeleteRepetitionNodeAll {
+	//NC25 删除有序链表中重复的元素(都删除）
+	//样例：给出的链表为1 1 2 3 3 返回 2
+	//分析：删除给出链表中的重复元素（链表中元素从小到大有序），使链表中相同的都删除
+	//思路：创建一个亚节点cur，从head开始遍历，如果head.val == head.next.val那么接一个while循环，如果还相等，head= head.next;如果不相等了，那么head后移以为，然后cur.next = head;
+	//                                  如果不相等，那么head后移，cur后移即可
+	public static void main(String[] args) {
+		ListNode node1 = new ListNode(1);
+		ListNode node2 = new ListNode(1);
+		ListNode node3 = new ListNode(2);
+		ListNode node4 = new ListNode(3);
+		ListNode node5 = new ListNode(3);
+		node1.next = node2;
+		node2.next = node3;
+		node3.next = node4;
+		node4.next = node5;
+
+		ListNode res1 = deleteDuplicates(node1);
+		while (res1 != null) {
+			System.out.print(res1.val + " ");
+			res1 = res1.next;
+		}
+	}
+
+	public static ListNode deleteDuplicates(ListNode head) {
+		ListNode dummy = new ListNode(-1);
+		dummy.next = head;
+		ListNode cur = dummy;
+		while (head != null) {
+			if (head.next != null && head.val == head.next.val) {
+				while (head.next != null && head.val == head.next.val) {
+					head = head.next;
+				}
+				head = head.next;
+				cur.next = head;
+			} else {
+				head = head.next;
+				cur = cur.next;
+			}
+		}
+		return dummy.next;
+	}
 }
