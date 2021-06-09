b14e7193ca4236c747c2d30a7796ca3ed06be950
xubin03 <xubin03@baijia.com>
2021-06-10 02:17:48 +0800

    string

---
+++ src/main/java/com/bingoabin/algorithm/string/MaxLenBucketSubString.java
@@ -0,0 +1,19 @@
+package com.bingoabin.algorithm.string;
+
+/**
+ * @author xubin03
+ * @date 2021/6/10 2:16 上午
+ */
+public class MaxLenBucketSubString {
+	//NC49 最长的括号子串
+	//样例：输入")()())" 输出：4
+	//分析：仅包含字符'('和')'的字符串，计算最长的格式正确的括号子串的长度
+	//思路：方式一：
+	public static void main(String[] args) {
+
+	}
+
+	public static int longestValidParentheses1(String s) {
+		return 1;
+	}
+}
