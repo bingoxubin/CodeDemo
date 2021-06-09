f166a4f08b1a3b3b20f0e5a4e1914106993e173c
xubin03 <xubin03@baijia.com>
2021-06-09 19:40:45 +0800

    string

---
+++ src/main/java/com/bingoabin/algorithm/string/LongestCommonPrefix.java
@@ -0,0 +1,22 @@
+package com.bingoabin.algorithm.string;
+
+/**
+ * @author xubin03
+ * @date 2021/6/9 7:36 下午
+ */
+public class LongestCommonPrefix {
+	//NC55 最长公共前缀
+	//样例：输入["abca","abc","abca","abc","abcc"] 输出：abc
+	//分析：查找字符串数组中的最长公共前缀。
+	//思路：
+	public static void main(String[] args) {
+		String[] res = {"abca", "abc", "abca", "abc", "abcc"};
+		System.out.println(longestCommonPrefix(res));
+	}
+
+	public static String longestCommonPrefix(String[] strs) {
+
+
+		return "";
+	}
+}
