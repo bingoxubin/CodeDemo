9b72575276f13676eb1e80cb2992358699d53b81
bingoxubin <757661238@qq.com>
2021-07-03 01:25:54 +0800

    maxicecream

--- src/main/java/com/bingoabin/designpattern/behavior/Iterator.java
+++ src/main/java/com/bingoabin/designpattern/behavior/Iterator.java
@@ -5,7 +5,7 @@ package com.bingoabin.designpattern.behavior;
  * @date 2021/6/15 1:47
  */
 public class Iterator {
-	//迭代器模式：
+	//迭代器模式：是一种行为型设计模式，让你能再不暴露集合底层表现形式（列表，栈，树等）的情况下遍历集合中的所有的元素
 	public static void main(String[] args){
 
 	}
