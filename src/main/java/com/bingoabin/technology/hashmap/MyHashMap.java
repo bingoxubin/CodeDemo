83d32f8d98c18d9191fbea8c83ddf9127c5ea984
xubin34 <xubin34@meituan.com>
2021-07-17 18:41:53 +0800

    hashmap

--- src/main/java/com/bingoabin/technology/hashmap/MyHashMap.java
+++ src/main/java/com/bingoabin/technology/hashmap/MyHashMap.java
@@ -22,16 +22,10 @@ public class MyHashMap {
 	Node[] map;
 	int len = 10000;
 
-	/**
-	 * Initialize your data structure here.
-	 */
 	public MyHashMap() {
 		map = new Node[len];
 	}
 
-	/**
-	 * value will always be non-negative.
-	 */
 	public void put(int key, int value) {
 		int index = key % len;
 		if (map[index] == null) {
@@ -50,9 +44,6 @@ public class MyHashMap {
 		}
 	}
 
-	/**
-	 * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
-	 */
 	public int get(int key) {
 		int index = key % len;
 		if (map[index] != null) {
@@ -67,9 +58,6 @@ public class MyHashMap {
 		return -1;
 	}
 
-	/**
-	 * Removes the mapping of the specified value key if this map contains a mapping for the key
-	 */
 	public void remove(int key) {
 		int index = key % len;
 		if (map[index] != null) {
