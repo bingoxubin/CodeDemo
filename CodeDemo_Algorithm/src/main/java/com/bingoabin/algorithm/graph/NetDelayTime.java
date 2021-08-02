26b5d198fb29393eac0bd97ee8a47cb595692dba
xubin34 <xubin34@meituan.com>
2021-08-03 03:11:38 +0800

    NetDelayTime

---
+++ CodeDemo_Algorithm/src/main/java/com/bingoabin/algorithm/graph/NetDelayTime.java
@@ -0,0 +1,16 @@
+package com.bingoabin.algorithm.graph;
+
+/**
+ * @author xubin34
+ * @date 2021/8/3 3:06 上午
+ */
+public class NetDelayTime {
+	//Leetcode 743. 网络延迟时间
+	//示例：输入：times = [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2  输出：2
+	//分析：有n个节点，标记为1到n，给一个列表times 其中一个元素表示，从一个节点开始，到另一个节点结束，以及需要的时间，times[i] = (开始点，结束点，时间）
+	//     现在指定从k节点发出信号，求多久所有节点都能收到信号，如果不能使所有节点收到信号，返回-1
+	//思路：https://leetcode-cn.com/problems/network-delay-time/solution/java-ban-dijkstra-spfa-floydxiang-xi-ti-jie-by-jer/
+	public static void main(String[] args) {
+
+	}
+}
