96469498a7e1008c19c7365e58290384d8a1f2b4
xubin34 <xubin34@meituan.com>
2021-08-11 00:54:57 +0800

    loggertest

--- CodeDemo_JVM/src/main/java/com/bingoabin/classstored/TestClass.java
+++ CodeDemo_JVM/src/main/java/com/bingoabin/classstored/TestClass.java
@@ -1,9 +1,13 @@
-package com.bingoabin.classstored;/**
-*
-* @Author: xubin34
-* @Date: 2021/8/10 1:51 上午
-* @CopyRight: sankuai.com
-* @Description: 
-*/
+package com.bingoabin.classstored;
+
+import java.util.Date;
+
+/**
+ * @Author: xubin34
+ * @Date: 2021/8/10 1:51 上午
+ * @CopyRight: sankuai.com
+ * @Description:
+ */
 public class TestClass {
+	private Date data = new Date();
 }
--- CodeDemo_Java/src/main/java/com/bingoabin/logger/LoggerTest.java
+++ CodeDemo_Java/src/main/java/com/bingoabin/logger/LoggerTest.java
@@ -1,9 +1,35 @@
-package com.bingoabin.logger;/**
-*
-* @Author: xubin34
-* @Date: 2021/8/10 8:12 下午
-* @CopyRight: sankuai.com
-* @Description: 
-*/
+package com.bingoabin.logger;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * @Author: xubin34
+ * @Date: 2021/8/10 8:12 下午
+ * @CopyRight: sankuai.com
+ * @Description:
+ */
 public class LoggerTest {
+	static Logger logger = LogManager.getLogger(LoggerTest.class);
+
+	public static void main(String[] args) {
+		int a = 10;
+		if (a == 10) {
+			logger.error("a的值不对啊:{}", a);
+		}
+		System.out.println("helloworld");
+		//ERROR StatusLogger No log4j2 configuration file found. Using default configuration: logging only errors to the console. Set system property 'log4j2.debug' to show Log4j2 internal initialization logging.
+		// 20:17:08.069 [main] ERROR com.bingoabin.logger.LoggerTest - a的值不对啊:10
+		// helloworld
+
+		try {
+			a = 10 / 0;
+		} catch (Exception e) {
+			logger.error("分母出现0：{}", a, e);
+		}
+		//20:19:28.911 [main] ERROR com.bingoabin.logger.LoggerTest - 分母出现0：10
+		// java.lang.ArithmeticException: / by zero
+		// 	at com.bingoabin.logger.LoggerTest.main(LoggerTest.java:26) [classes/:?]
+	}
+
 }
