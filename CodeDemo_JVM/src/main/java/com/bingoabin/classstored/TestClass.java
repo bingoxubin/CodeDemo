4d3176593cc5a8adf68af6067a15e941057cdd1d
xubin34 <xubin34@meituan.com>
2021-08-10 22:04:51 +0800

    loggertest

---
+++ CodeDemo_JVM/src/main/java/com/bingoabin/classstored/TestClass.java
@@ -0,0 +1,9 @@
+package com.bingoabin.classstored;/**
+*
+* @Author: xubin34
+* @Date: 2021/8/10 1:51 上午
+* @CopyRight: sankuai.com
+* @Description: 
+*/
+public class TestClass {
+}
---
+++ CodeDemo_Java/src/main/java/com/bingoabin/logger/LoggerTest.java
@@ -0,0 +1,9 @@
+package com.bingoabin.logger;/**
+*
+* @Author: xubin34
+* @Date: 2021/8/10 8:12 下午
+* @CopyRight: sankuai.com
+* @Description: 
+*/
+public class LoggerTest {
+}
