ec964a89389613403502f5534f95938ea3c1b1de
xubin34 <xubin34@meituan.com>
2021-08-11 10:19:31 +0800

    springpool

--- CodeDemo_JVM/src/main/java/com/bingoabin/stringpool/StringPoolTest4.java
+++ CodeDemo_JVM/src/main/java/com/bingoabin/stringpool/StringPoolTest4.java
@@ -1,11 +1,10 @@
 package com.bingoabin.stringpool;
 
-import java.util.Random;
-import java.lang.String;
+import java.security.SecureRandom;
 public class StringPoolTest4 {
 
 	public static void main(String[] args) throws Exception {
-		Random random = new Random(10);
+		SecureRandom random = new SecureRandom();
 		String s1 = new String(random.nextInt() + "").intern();
 		String s2 = new String(random.nextInt() + "").intern();
 		System.out.println(s1 == s2);
