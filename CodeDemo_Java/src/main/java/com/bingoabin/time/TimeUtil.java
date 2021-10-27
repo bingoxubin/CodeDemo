4cee866db301e04c8eecf719dc0e7e2eaac60f23
xubin34 <xubin34@meituan.com>
2021-10-27 10:14:54 +0800

    UTC时间转化成localDate

--- CodeDemo_Java/src/main/java/com/bingoabin/time/TimeUtil.java
+++ CodeDemo_Java/src/main/java/com/bingoabin/time/TimeUtil.java
@@ -1,5 +1,7 @@
 package com.bingoabin.time;
 
+import org.apache.logging.log4j.core.util.datetime.FastDateFormat;
+
 import java.text.SimpleDateFormat;
 
 /**
@@ -15,9 +17,18 @@ public class TimeUtil {
 		System.out.println(timeReverse.test("2021-02-05T18:49:33.183000+08:00"));
 		System.out.println(timeReverse.test("2021-02-05T18:49:33.18+08:00"));
 		System.out.println(timeReverse.test("2021-02-05T18:49:33.10+08:00"));
+
+		System.out.println(timeReverse.test1("2021-02-05T18:49:33.182888+08:00"));
+		System.out.println(timeReverse.test1("2021-02-05T18:49:33.183000+08:00"));
+		System.out.println(timeReverse.test1("2021-02-05T18:49:33.18+08:00"));
+		System.out.println(timeReverse.test1("2021-02-05T18:49:33.10+08:00"));
 	}
 
 	public String test(String stringTime) throws Exception{
 		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS").parse(stringTime));
+	}
+
+	public String test1(String str1) throws Exception {
+		return FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss").format(FastDateFormat.getInstance("yyyy-MM-dd'T'HH:mm:ss.SSSSSS").parse(str1));
 	}
 }
