44b969827347d4e8069a77e2cdd52ec348908583
xubin34 <xubin34@meituan.com>
2021-11-07 12:17:21 +0800

    Leetcode 598. 范围求和 II

---
+++ CodeDemo_ZTest/src/main/java/com/bingoabin/test/TestYuan.java
@@ -0,0 +1,20 @@
+package com.bingoabin.test;
+
+import static java.lang.System.*;
+
+/**
+ * @Author: xubin34
+ * @Date: 2021/11/6 12:50 下午
+ * @CopyRight: sankuai.com
+ * @Description:
+ */
+public class TestYuan {
+	public static void main(String[] args) {
+		TestYuan testYuan = new TestYuan();
+		testYuan.add(30, 50);
+	}
+
+	public void add(int a, int b) {
+		out.println(a + b);
+	}
+}
