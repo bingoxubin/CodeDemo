10dde43dd691c73f5743ba042be4ecaafa570053
xubin34 <xubin34@meituan.com>
2021-11-20 10:55:04 +0800

    测试&

---
+++ CodeDemo_ZTest/src/main/java/com/bingoabin/test/TestMath.java
@@ -0,0 +1,14 @@
+package com.bingoabin.test;
+
+/**
+ * @Author: xubin34
+ * @Date: 2021/11/19 10:46 上午
+ * @CopyRight: sankuai.com
+ * @Description:
+ */
+public class TestMath {
+	public static void main(String[] args) {
+		System.out.println(8 >> 1);
+		System.out.println(((8 >> 1) & 1));   // 0 & 任意  都是 0
+	}
+}
