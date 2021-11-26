a0e2d06989883a0d094d54a18686f762f65ee2a6
xubin34 <xubin34@meituan.com>
2021-11-26 19:59:44 +0800

    java8 lazy

---
+++ CodeDemo_Java/src/main/java/com/bingoabin/java8new/_3LazyTest.java
@@ -0,0 +1,16 @@
+package com.bingoabin.java8new;
+
+/**
+ * @Author: xubin34
+ * @Date: 2021/11/26 6:33 下午
+ * @CopyRight: sankuai.com
+ * @Description:
+ */
+public class _3LazyTest {
+	//利用Stream类型的"懒"操作
+	//代码中的很多操作都是Eager的，比如在发生方法调用的时候，参数会立即被求值。总体而言，使用Eager方式让编码本身更加简单，然而使用Lazy的方式通常而言，即意味着更好的效率。
+
+	public static void main(String[] args) {
+
+	}
+}
