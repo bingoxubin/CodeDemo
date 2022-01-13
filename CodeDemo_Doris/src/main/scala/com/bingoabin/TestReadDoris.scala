a34b3dcf62be48582ef992c109ce5ce4924df6b3
xubin34 <xubin34@meituan.com>
2022-01-13 11:58:39 +0800

    doris写入读取数据

---
+++ CodeDemo_Doris/src/main/scala/com/bingoabin/TestReadDoris.scala
@@ -0,0 +1,45 @@
+package com.bingoabin
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+import java.util.Properties
+
+//从doris中读取数据
+object TestReadDoris {
+  def main(args: Array[String]): Unit = {
+    //1、创建SparkConf对象
+    val sparkConf: SparkConf = new SparkConf().setAppName("testReadDoris").setMaster("local[*]")
+
+    //2、创建SparkSession对象
+    val spark: SparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
+
+    //3、读取mysql表的数据
+    //3.1 指定mysql连接地址
+    val url = "jdbc:mysql://localhost:3306/doris?characterEncoding=UTF-8"
+    //3.2 指定要加载的表名
+    val tableName = "doris"
+    // 3.3 配置连接数据库的相关属性
+    val properties = new Properties()
+
+    //用户名
+    properties.setProperty("user", "root")
+    //密码
+    properties.setProperty("password", "111111")
+
+    val mysqlDF = spark.read.jdbc(url, tableName, properties)
+
+    //打印schema信息
+    mysqlDF.printSchema()
+
+    //展示数据
+    mysqlDF.show()
+
+    //把dataFrame注册成表
+    mysqlDF.createTempView("doris")
+
+    spark.sql("select * from doris").show()
+
+    spark.stop()
+  }
+}
---
+++ CodeDemo_Doris/src/main/scala/com/bingoabin/TestWriteDoris.scala
@@ -0,0 +1,23 @@
+package com.bingoabin
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{SaveMode, SparkSession}
+
+import java.util.Properties
+
+//从doris中写入数据
+object TestWriteDoris {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setAppName("testWriteDoris").setMaster("local[*]")
+    val sparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
+    val df = sparkSession
+      .createDataFrame(Array((4, "2020-09-13", "testName1", 18), (5, "2020-09-13", "testName2", 18), (6, "2020-09-13", "testName3", 18)))
+      .toDF("id", "time", "name", "age")
+    val prop = new Properties()
+    prop.setProperty("user", "root")
+    prop.setProperty("password", "111111")
+    //doris地址 "http://doris1:9030/doris"，跟操作mysql一致
+    df.write.mode(SaveMode.Append).jdbc("jdbc:mysql://localhost:3306/doris", "doris", prop)
+    print("写入成功！")
+  }
+}
