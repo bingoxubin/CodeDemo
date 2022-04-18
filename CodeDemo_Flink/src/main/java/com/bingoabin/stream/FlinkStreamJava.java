a29ed67fbbaa4f24e7ec19a307e3c6cc5795448f
bingoabin <757661238@qq.com>
2022-04-18 14:52:04 +0800

    flink

--- CodeDemo_Flink/src/main/java/com/bingoabin/stream/FlinkStreamJava.java
+++ CodeDemo_Flink/src/main/java/com/bingoabin/stream/FlinkStreamJava.java
@@ -6,6 +6,8 @@ package com.bingoabin.stream;
  */
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
