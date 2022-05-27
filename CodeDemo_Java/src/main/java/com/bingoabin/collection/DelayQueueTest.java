984a5c10c53d15cf79c6fdc9020b40006507fe84
bingoabin <757661238@qq.com>
2022-05-27 08:59:11 +0800

    DelayQueue

---
+++ CodeDemo_Java/src/main/java/com/bingoabin/collection/DelayQueueTest.java
@@ -0,0 +1,62 @@
+package com.bingoabin.collection;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author bingoabin
+ * @date 2022/5/27 8:55
+ */
+public class DelayQueueTest {
+	static BlockingQueue<Delayed> queue = new DelayQueue();
+	public static void main(String[] args) throws InterruptedException {
+		queue.add(new MyDelay(80000, "第一次添加任务"));
+		queue.add(new MyDelay(30000, "第二次添加任务"));
+		queue.add(new MyDelay(50000, "第三次添加任务"));
+
+		while (!queue.isEmpty()) {
+			Delayed delayed = queue.take();
+			System.out.println(delayed);
+		}
+	}
+}
+
+class MyDelay<T> implements Delayed {
+
+	long delayTime; // 延迟时间
+	long expire; // 过期时间
+	T data;
+
+	public MyDelay(long delayTime, T t) {
+		this.delayTime = delayTime;
+		// 过期时间 = 当前时间 + 延迟时间
+		this.expire = System.currentTimeMillis() + delayTime;
+		data = t;
+	}
+
+	/**
+	 * 剩余时间 = 到期时间 - 当前时间
+	 */
+	@Override
+	public long getDelay(TimeUnit unit) {
+		return unit.convert(this.expire - System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+	}
+
+	/**
+	 * 优先级规则：两个任务比较，时间短的优先执行
+	 */
+	@Override
+	public int compareTo(Delayed o) {
+		long f = this.getDelay(TimeUnit.MILLISECONDS) - o.getDelay(TimeUnit.MILLISECONDS);
+		return (int) f;
+	}
+
+	@Override
+	public String toString() {
+		return "delayTime=" + delayTime +
+				", expire=" + expire +
+				", data=" + data;
+	}
+}
---
+++ CodeDemo_ZTest/src/main/java/com/bingoabin/test/TestHashMap.java
@@ -0,0 +1,27 @@
+package com.bingoabin.test;
+
+import com.bingoabin.domain.Person;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author bingoabin
+ * @date 2022/5/24 20:36
+ */
+public class TestHashMap {
+	public static void main(String[] args){
+		Person person1 = new Person(1, "xb");
+		Person person2 = new Person(2, "zhangsan");
+		Person person3 = new Person(3, "lisi");
+		Map<Person, Integer> map = new HashMap<>();
+
+		map.put(person1,2);
+		map.put(person2, 3);
+		map.put(person3, 4);
+		System.out.println(map.get(person1));
+		person1.setId(2);
+		System.out.println(map.get(person1));
+
+	}
+}
