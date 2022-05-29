0acc529b1bb23ab2d379e99125171d796d842123
bingoabin <757661238@qq.com>
2022-05-29 12:03:04 +0800

    synchronized

--- CodeDemo_JUC/src/main/java/com/bingoabin/deadlocktest/DeadLock.java
+++ CodeDemo_JUC/src/main/java/com/bingoabin/deadlocktest/DeadLock.java
@@ -1,5 +1,12 @@
 package com.bingoabin.deadlocktest;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author bingoabin
  * @date 2022/5/29 11:33
@@ -7,6 +14,22 @@ package com.bingoabin.deadlocktest;
 public class DeadLock {
 	public static void main(String[] args) {
 		DeadLock.deadLock();
+		ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+		Runnable thread = new Runnable() {
+			@Override
+			public void run() {
+				long[] deadlockedThreads = threadMXBean.findDeadlockedThreads();
+				if (deadlockedThreads != null) {
+					ThreadInfo[] threadInfo = threadMXBean.getThreadInfo(deadlockedThreads);
+					System.out.println("检测到的所有的死锁如下：");
+					for (ThreadInfo info : threadInfo) {
+						System.out.println(info.getThreadId() + ":" + info.getThreadName());
+					}
+				}
+			}
+		};
+		ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
+		service.scheduleAtFixedRate(thread, 5, 5, TimeUnit.SECONDS);
 	}
 
 	public static void deadLock() {
