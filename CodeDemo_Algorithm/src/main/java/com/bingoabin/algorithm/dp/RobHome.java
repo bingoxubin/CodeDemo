ea30ddf404c9ccdc3f758a546594571a1390300d
bingoabin <757661238@qq.com>
2022-06-30 16:18:55 +0800

    hammingweight

--- CodeDemo_Algorithm/src/main/java/com/bingoabin/algorithm/dp/RobHome.java
+++ CodeDemo_Algorithm/src/main/java/com/bingoabin/algorithm/dp/RobHome.java
@@ -9,28 +9,43 @@ public class RobHome {
 	//样例：[2,7,9,3,1] 返回 12
 	//分析：数组中相邻的不能取，取的最大和
 	//思路：用dp数组，记录最大值dp[0]=arr[0];dp[1]=Math.max(arr[0],arr[1];dp[2]=Math.max(dp[0]+dp[2],dp[1])
-	public static void main(String[] args){
-	    int[] arr = {2,7,9,3,1};
+	public static void main(String[] args) {
+		int[] arr = {2, 7, 9, 3, 1};
+		System.out.println(rob(arr));
 		System.out.println(rob(arr));
 	}
 
-	public static int rob(int[] arr){
+	public static int rob(int[] arr) {
 		int len = arr.length;
-		if(len == 0) {
+		if (len == 0) {
 			return 0;
 		}
-		if(len == 1) {
+		if (len == 1) {
 			return arr[0];
 		}
-		if(len == 2) {
-			return Math.max(arr[0],arr[1]);
+		if (len == 2) {
+			return Math.max(arr[0], arr[1]);
 		}
 		int[] dp = new int[len];
 		dp[0] = arr[0];
-		dp[1] = Math.max(arr[1],arr[0]);
-		for(int i = 2;i<len;i++){
-			dp[i] = Math.max(arr[i]+dp[i-2],dp[i-1]);
+		dp[1] = Math.max(arr[1], arr[0]);
+		for (int i = 2; i < len; i++) {
+			dp[i] = Math.max(arr[i] + dp[i - 2], dp[i - 1]);
 		}
-		return dp[len-1];
+		return dp[len - 1];
+	}
+
+	public static int rob1(int[] arr) {
+		int len = arr.length;
+		if (len == 0) return 0;
+		if (len == 1) return arr[0];
+		if (len == 2) return Math.max(arr[0], arr[1]);
+		int[] dp = new int[len];
+		dp[0] = arr[0];
+		dp[1] = Math.max(arr[1], arr[0]);
+		for (int i = 2; i < len; i++) {
+			dp[i] = Math.max(arr[i] + dp[i - 2], dp[i - 1]);
+		}
+		return dp[len - 1];
 	}
 }
