21d6f2fc772e551e65149931aeb6bb0c784b2fdb
bingoabin <757661238@qq.com>
2022-07-07 14:44:41 +0800

    FindLHS

--- CodeDemo_Algorithm/src/main/java/com/bingoabin/algorithm/hashmap/FindLHS.java
+++ CodeDemo_Algorithm/src/main/java/com/bingoabin/algorithm/hashmap/FindLHS.java
@@ -23,6 +23,7 @@ public class FindLHS {
 		FindLHS findLHS = new FindLHS();
 		out.println(findLHS.findLHS1(nums));
 		out.println(findLHS.findLHS2(nums));
+		out.println(findLHS.findLHS(nums));
 	}
 
 	public int findLHS1(int[] nums) {
@@ -54,6 +55,20 @@ public class FindLHS {
 			//如果存在num-1 统计次数
 			if (map.containsKey(num - 1)) {
 				res = Math.max(res, map.get(num) + map.get(num - 1));
+			}
+		}
+		return res;
+	}
+
+	public int findLHS(int[] nums) {
+		Map<Integer, Integer> map = new HashMap<>();
+		int res = 0;
+		for (int num : nums) {
+			map.put(num, map.getOrDefault(num, 0) + 1);
+		}
+		for (int key : map.keySet()) {
+			if (map.containsKey(key + 1)) {
+				res = Math.max(res, map.get(key) + map.get(key + 1));
 			}
 		}
 		return res;
