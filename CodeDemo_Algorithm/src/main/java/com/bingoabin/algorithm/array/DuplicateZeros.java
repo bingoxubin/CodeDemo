0d7af8435b8b83db26e61fb436d111175658e690
bingoabin <757661238@qq.com>
2022-07-08 10:15:56 +0800

    DuplicateZeros

--- CodeDemo_Algorithm/src/main/java/com/bingoabin/algorithm/array/DuplicateZeros.java
+++ CodeDemo_Algorithm/src/main/java/com/bingoabin/algorithm/array/DuplicateZeros.java
@@ -37,4 +37,15 @@ public class DuplicateZeros {
 			}
 		}
 	}
+
+	public void duplicatedZeros(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 0 && i < arr.length - 1) {
+				for (int j = arr.length - 2; j >= i + 1; j--) {
+					arr[j + 1] = arr[j];
+				}
+				arr[++i] = 0;
+			}
+		}
+	}
 }
