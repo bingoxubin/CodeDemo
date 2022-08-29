d454ad5f97a9279c1c293a70a8f3ceab6dbd2166
bingoabin <757661238@qq.com>
2022-08-29 21:08:25 +0800

    TestPattern

---
+++ CodeDemo_ZTest/src/main/java/com/bingoabin/test/TestMatching.java
@@ -0,0 +1,18 @@
+package com.bingoabin.test;
+
+import java.util.regex.Pattern;
+
+/**
+ * @author bingoabin
+ * @date 2022/8/29 20:56
+ * @Description: 测试match
+ */
+public class TestMatching {
+	public static void main(String[] args){
+		String s1 = "C)Q";
+		boolean matches1 = Pattern.compile("C\\)Q").matcher(s1).matches();
+		System.out.println(matches1);
+		// boolean matches = s1.matches(s2);
+		// System.out.println(matches);
+	}
+}
