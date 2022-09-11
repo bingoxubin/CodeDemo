50e5701e302468495e8a80ef7bb84c3d230f458f
bingoabin <757661238@qq.com>
2022-09-11 11:32:43 +0800

    SingleTon

--- CodeDemo_DesignPattern/src/main/java/com/bingoabin/createtable/SingleTon.java
+++ CodeDemo_DesignPattern/src/main/java/com/bingoabin/createtable/SingleTon.java
@@ -10,6 +10,7 @@ public class SingleTon {
 }
 
 //饿汉式:在类加载的时候就进行实例化
+//可用
 class SingleTon1 {
 	private static SingleTon1 singleTon1 = new SingleTon1();
 
@@ -22,6 +23,7 @@ class SingleTon1 {
 }
 
 //懒汉式：类加载的时候不进行实例化，在第一次使用的时候进行实例化
+//可用
 class SingleTon2 {
 	private static SingleTon2 singleTon2;
 
@@ -41,6 +43,7 @@ class SingleTon2 {
 //2. 初始化对象
 //3.指向分配的地址
 //若发生重排序，假设A线程执行了1和3，还没有执行2，B线程来到判断NULL，B线程就会直接返回还没初始化的instance了，volatile可以避免重排序
+//(可用，推荐）
 class SingleTon3 {
 	private volatile static SingleTon3 singleTon3;
 
@@ -48,9 +51,9 @@ class SingleTon3 {
 	}
 
 	public static SingleTon3 getInstance() {
-		if (singleTon3 == null) {
+		if (singleTon3 == null) {   //为了提升效率，不用每一次都去竞争锁
 			synchronized (SingleTon.class) {
-				if (singleTon3 == null) {
+				if (singleTon3 == null) {  //如果两个线程进来，一个线程创建了实例，另外一个线程还能创建
 					singleTon3 = new SingleTon3();
 				}
 			}
@@ -62,3 +65,22 @@ class SingleTon3 {
 //一下两种情况还会产生多实例
 //1.分布式环境中，多个jvm虚拟机
 //2.一个jvm虚拟机，多个类加载器
+
+//枚举类实现(可用，推荐）
+enum Singleton {
+	INSTANCE;
+}
+
+//静态内部类(可用，推荐）
+class SingleTon4 {
+	private SingleTon4() {
+	}
+
+	public static SingleTon4 getInstance() {
+		return SingleTonInstance.INSTANCE;
+	}
+
+	private static class SingleTonInstance {
+		private static final SingleTon4 INSTANCE = new SingleTon4();
+	}
+}
