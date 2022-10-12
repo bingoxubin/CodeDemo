2b2cd4ed79a7d0b253af08f7aabee45163bdb2db
bingoabin <757661238@qq.com>
2022-10-12 20:43:51 +0800

    MaxSwap

--- CodeDemo_Algorithm/src/main/java/com/bingoabin/algorithm/greedy/MaxSwap.java
+++ CodeDemo_Algorithm/src/main/java/com/bingoabin/algorithm/greedy/MaxSwap.java
@@ -11,10 +11,13 @@ public class MaxSwap {
 	public static void main(String[] args) {
 		MaxSwap maxSwap = new MaxSwap();
 		System.out.println(maxSwap.maximumSwap(326587));
+		System.out.println(maxSwap.maximumSwap(267743));
+		System.out.println(maxSwap.maximumSwap1(326587));
+		System.out.println(maxSwap.maximumSwap1(267743));
 	}
 
 	//贪心算法
-	public int maximumSwap(int num) {
+	public int maximumSwap1(int num) {
 		String str = String.valueOf(num);
 		char[] ch = str.toCharArray();
 		int[] dp = new int[ch.length];
@@ -26,6 +29,30 @@ public class MaxSwap {
 			dp[i] = maxIndex;
 		}
 		System.out.println(Arrays.toString(dp));
+		for (int i = 0; i < ch.length; i++) {
+			if (ch[i] != ch[dp[i]]) {
+				char temp = ch[dp[i]];
+				ch[dp[i]] = ch[i];
+				ch[i] = temp;
+				break;
+			}
+		}
+		return Integer.parseInt(new String(ch));
+	}
+
+	//自己实现一遍
+	public int maximumSwap(int num) {
+		String nums = String.valueOf(num);
+		char[] ch = nums.toCharArray();
+		int[] dp = new int[ch.length];
+		int maxIndex = ch.length - 1;
+		for (int i = ch.length - 1; i >= 0; i--) {
+			if (ch[i] > ch[maxIndex]) {
+				maxIndex = i;
+			}
+			dp[i] = maxIndex;
+		}
+
 		for (int i = 0; i < ch.length; i++) {
 			if (ch[i] != ch[dp[i]]) {
 				char temp = ch[dp[i]];
