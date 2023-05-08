7e27e218b0f76b89d0f3163928630d4a3a65e882
bingo abin <757661238@qq.com>
2023-05-08 22:42:05 +0800

    two sum

---
+++ CodeDemo_Algorithm/src/main/java/com/bingoabin/leetcode/_0001TwoSum.java
@@ -0,0 +1,22 @@
+package com.bingoabin.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author bingoabin
+ * @date 2023/5/8 22:34
+ * @Description:
+ */
+public class _0001TwoSum {
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			if (map.containsKey(target - nums[i])) {
+				return new int[]{map.get(target - nums[i]), i};
+			}
+			map.put(nums[i], i);
+		}
+		return new int[]{-1, -1};
+	}
+}
