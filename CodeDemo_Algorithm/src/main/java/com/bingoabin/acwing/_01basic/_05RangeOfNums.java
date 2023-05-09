ceff32bbae496af5f44d739954020bcfd6d1d47d
bingo abin <757661238@qq.com>
2023-05-09 16:12:52 +0800

    RangeOfNums

--- CodeDemo_Algorithm/src/main/java/com/bingoabin/acwing/_01basic/_05RangeOfNums.java
+++ CodeDemo_Algorithm/src/main/java/com/bingoabin/acwing/_01basic/_05RangeOfNums.java
@@ -9,6 +9,10 @@ import java.util.Scanner;
  */
 public class _05RangeOfNums {
 	public static void main(String[] args) {
+		//测试
+		System.out.println(getLessThanMax(new int[]{4, 5, 5, 5, 5, 5, 6, 8, 9}, 5));
+		System.out.println(getMoreThanMin(new int[]{4, 5, 5, 5, 5, 5, 6, 8, 9}, 5));
+
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int nums = sc.nextInt();
@@ -18,19 +22,48 @@ public class _05RangeOfNums {
 		}
 		for (int i = 0; i < nums; i++) {
 			int num = sc.nextInt();
-
+			int lessThanMax = getLessThanMax(arr, num);
+			int moreThanMin = getMoreThanMin(arr, num);
+			if (lessThanMax == -1 || moreThanMin == -1 || arr[lessThanMax] != num || arr[moreThanMin] != num) {
+				System.out.printf("%d %d", -1, -1);
+				System.out.println();
+			} else {
+				System.out.printf("%d %d", moreThanMin, lessThanMax);
+			}
 		}
 	}
 
 	//小于等于目标值的最大下标
 	public static int getLessThanMax(int[] arr, int num) {
-
-		return -1;
+		int left = 0;
+		int right = arr.length - 1;
+		int res = -1;
+		while (left <= right) {
+			int mid = (right - left) / 2 + left;
+			if (arr[mid] <= num) {
+				res = mid;
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		return res;
 	}
 
 	//大于等于目标值的最小下标
 	public static int getMoreThanMin(int[] arr, int num) {
-
-		return -1;
+		int left = 0;
+		int right = arr.length - 1;
+		int res = -1;
+		while (left <= right) {
+			int mid = (right - left) / 2 + left;
+			if (arr[mid] >= num) {
+				res = mid;
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		}
+		return res;
 	}
 }
