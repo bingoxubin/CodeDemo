e1ecbf9802f0664510c3e0081a99811003149c14
bingo abin <757661238@qq.com>
2023-05-09 15:55:29 +0800

    RangeOfNums

--- CodeDemo_Algorithm/src/main/java/com/bingoabin/acwing/_01basic/_05RangeOfNums.java
+++ CodeDemo_Algorithm/src/main/java/com/bingoabin/acwing/_01basic/_05RangeOfNums.java
@@ -8,19 +8,29 @@ import java.util.Scanner;
  * @Description:
  */
 public class _05RangeOfNums {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int nums = sc.nextInt();
 		int[] arr = new int[n];
-		for(int i = 0;i<n;i++){
+		for (int i = 0; i < n; i++) {
 			arr[i] = sc.nextInt();
 		}
-		for(int i = 0;i<nums;i++){
+		for (int i = 0; i < nums; i++) {
 			int num = sc.nextInt();
 
 		}
 	}
-	
 
+	//小于等于目标值的最大下标
+	public static int getLessThanMax(int[] arr, int num) {
+
+		return -1;
+	}
+
+	//大于等于目标值的最小下标
+	public static int getMoreThanMin(int[] arr, int num) {
+
+		return -1;
+	}
 }
