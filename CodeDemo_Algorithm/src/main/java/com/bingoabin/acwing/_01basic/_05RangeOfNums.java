4597fcf5a8338125f2ab8a660b650be2785c8f8b
bingo abin <757661238@qq.com>
2023-05-09 12:44:28 +0800

    rangeofnums

---
+++ CodeDemo_Algorithm/src/main/java/com/bingoabin/acwing/_01basic/_05RangeOfNums.java
@@ -0,0 +1,26 @@
+package com.bingoabin.acwing._01basic;
+
+import java.util.Scanner;
+
+/**
+ * @author bingoabin
+ * @date 2023/5/9 11:00
+ * @Description:
+ */
+public class _05RangeOfNums {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int nums = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0;i<n;i++){
+			arr[i] = sc.nextInt();
+		}
+		for(int i = 0;i<nums;i++){
+			int num = sc.nextInt();
+
+		}
+	}
+	
+
+}
