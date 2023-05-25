d82df270286cff9e1f5f3d466ee9de94db63b120
bingo abin <757661238@qq.com>
2023-05-25 14:25:52 +0800

    Mysql连接

---
+++ CodeDemo_MySQL/src/main/java/com/bingo/jdbc/MysqlTest.java
@@ -0,0 +1,53 @@
+package com.bingo.jdbc;
+
+import java.sql.*;
+
+/**
+ * @author bingoabin
+ * @date 2023/5/25 14:13
+ * @Description:
+ */
+public class MysqlTest {
+	public static void main(String[] args) throws SQLException {
+		String mysqlHost = "localhost";
+		int mysqlPort = 3306;
+		String mysqlDatabase = "test";
+		String mysqlUser = "root";
+		String mysqlPassword = "111111";
+
+		String jdbcUrl = "jdbc:mysql://" + mysqlHost + ":" + mysqlPort + "/" + mysqlDatabase + "?useSSL=false";
+		String driverName = "com.mysql.cj.jdbc.Driver";
+
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		try {
+			Class.forName(driverName);
+			conn = DriverManager.getConnection(jdbcUrl, mysqlUser, mysqlPassword);
+
+			// 执行查询语句
+			String query = "SELECT * FROM score";
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(query);
+
+			// 处理查询结果
+			while (rs.next()) {
+				// 从结果集中获取数据
+				String name = rs.getString("name");
+				String kecheng = rs.getString("kecheng");
+				int fenshu = rs.getInt("fenshu");
+
+				// 打印或处理数据
+				System.out.println("Name: " + name + ", Kecheng: " + kecheng + ", Fenshu：" + fenshu);
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}finally {
+			// 关闭连接
+			rs.close();
+			stmt.close();
+			conn.close();
+		}
+	}
+}
