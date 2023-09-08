3e5d22fe05323cb49db1cb90948175df09b81cb2
bingo abin <757661238@qq.com>
2023-09-08 08:24:24 +0800

    flatmap

---
+++ CodeDemo_Spark3.x/src/main/scala/com/bingo/core/_03rdd/_02operator/_01transform/Spark05_Glom.scala
@@ -0,0 +1,16 @@
+package com.bingo.core._03rdd._02operator._01transform
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark05_Glom {
+	def main(args: Array[String]): Unit = {
+		val conf: SparkConf = new SparkConf().setMaster("local").setAppName("Glom")
+		val sc = new SparkContext(conf)
+		val rdd: RDD[Int] = sc.makeRDD(List(1, 2, 3, 4), 2)
+
+		rdd.collect().foreach(println)
+		sc.stop()
+	}
+
+}
