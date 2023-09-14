f8a09155b5741c0f622f14429a548df7c278cb1f
bingo abin <757661238@qq.com>
2023-09-14 08:53:30 +0800

    action

--- CodeDemo_Spark3.x/src/main/scala/com/bingo/core/_03rdd/_02operator/_02action/Spark04_CountByKey.scala
+++ CodeDemo_Spark3.x/src/main/scala/com/bingo/core/_03rdd/_02operator/_02action/Spark04_CountByKey.scala
@@ -1,5 +1,24 @@
 package com.bingo.core._03rdd._02operator._02action
 
-object Spark04_CountByKey {
+import org.apache.spark.{SparkConf, SparkContext}
 
+object Spark04_CountByKey {
+	def main(args: Array[String]): Unit = {
+		val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
+		val sc = new SparkContext(sparkConf)
+
+		//val rdd = sc.makeRDD(List(1,1,1,4),2)
+		val rdd = sc.makeRDD(List(
+			("a", 1), ("a", 2), ("a", 3)
+		))
+
+		// TODO - 行动算子
+
+		//val intToLong: collection.Map[Int, Long] = rdd.countByValue()
+		//println(intToLong)
+		val stringToLong: collection.Map[String, Long] = rdd.countByKey()
+		println(stringToLong)
+
+		sc.stop()
+	}
 }
