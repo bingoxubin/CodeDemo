217e4b3f1ca4667b8ac0e14fa291e9f63a9b2d0d
bingo abin <757661238@qq.com>
2023-09-15 06:53:51 +0800

    foreach

--- CodeDemo_Spark3.x/src/main/scala/com/bingo/core/_03rdd/_02operator/_02action/Spark06_Foreach.scala
+++ CodeDemo_Spark3.x/src/main/scala/com/bingo/core/_03rdd/_02operator/_02action/Spark06_Foreach.scala
@@ -1,5 +1,18 @@
 package com.bingo.core._03rdd._02operator._02action
 
-object Spark06_Foreach {
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
 
+object Spark06_Foreach {
+	def main(args: Array[String]): Unit = {
+		val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("Foreach")
+		val sc = new SparkContext(conf)
+
+		val rdd: RDD[Int] = sc.makeRDD(List(1, 2, 3, 4))
+		rdd.collect().foreach(println)
+		println("***********************")
+		rdd.foreach(println)
+
+		sc.stop()
+	}
 }
