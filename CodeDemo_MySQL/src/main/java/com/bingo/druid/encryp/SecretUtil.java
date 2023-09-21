c20d400af0b5bd4c31eb997ccc00937e4fe188e5
bingo abin <757661238@qq.com>
2023-09-21 14:05:32 +0800

    druid encryptdatasource

--- CodeDemo_MySQL/src/main/java/com/bingo/druid/encryp/SecretUtil.java
+++ CodeDemo_MySQL/src/main/java/com/bingo/druid/encryp/SecretUtil.java
@@ -12,9 +12,9 @@ import org.slf4j.LoggerFactory;
 public class SecretUtil {
 
 	private static final Logger logger = LoggerFactory.getLogger(SecretUtil.class);
-	private static final String publicKey = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAJ6+g3lLjeTDXngtutPnsDMOvz3c/M+ZlsD5JLXbVKTEmZbnz2zxjFZh0fPZ1gXf22qCDesvqeYtJjU0DYaw3hkCAwEAAQ==";
+	private static final String publicKey = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBANI/A62k04JsV70qGk3v1chYJTXDI8ntaWYmJL/2x1M1rd4hLz+imA3cF7fTtsnMqDObv3ls9Bfpg0CO0Qk26cECAwEAAQ==";
 
-	private static final String privateKey = "";
+	private static final String privateKey = "MIIBVgIBADANBgkqhkiG9w0BAQEFAASCAUAwggE8AgEAAkEA0j8DraTTgmxXvSoaTe/VyFglNcMjye1pZiYkv/bHUzWt3iEvP6KYDdwXt9O2ycyoM5u/eWz0F+mDQI7RCTbpwQIDAQABAkEApw2Mtac2q/W8DUDr36tGOUbYYgxoXmS2j4SVRLYnkJOv4AlFLr/EC6VIZXCM1ULYrhQ7GWjUIx76xrGvz+m4MQIhAPdecs6sN//o4Sw/KrWpOTfkyESDX7jy5B+a2uP6PkC1AiEA2ZT6MYSjfDz79jqybCX9fuJ+YqcFBLF8RdAHYWlcyF0CIQDdDpQ+SPzKPoxrBA/gw1cUseieAWDbYIOoErSloo1bOQIhAJiPXN5bokX6fLCpd3FdMeFF7lg9w8cGq5xHwOUMe4IBAiAeKGs5xWFFHoo3yIQNlnEDaCzZ1Huz3EPhALiKnjuVDQ==";
 
 	public static String encryptDatasource(String plainText) throws Exception {
 		logger.info("加密");
--- CodeDemo_MySQL/src/main/java/com/bingo/druid/encryp/Test.java
+++ CodeDemo_MySQL/src/main/java/com/bingo/druid/encryp/Test.java
@@ -1,13 +1,19 @@
 package com.bingo.druid.encryp;
 
+import com.alibaba.druid.filter.config.ConfigTools;
+
 /**
  * @author bingoabin
  * @date 2023/9/20 8:03
  * @Description:
  */
 public class Test {
-	public static void main(String[] args) throws Exception{
-		System.out.println(SecretUtil.decryptDatasource("iE90MMLY0kMd3aKpYFXVG0fRbpbmgz7hrMyVJ9qmrEcpik4PSdQITcd8hKzixEQSiwpJKQ89IIH+vreh0e/tnw=="));
+	public static void main(String[] args) throws Exception {
+		//生成公私钥
+		String[] keyPair = ConfigTools.genKeyPair(512);
+		System.out.println("Private Key: " + keyPair[0]);
+		System.out.println("Public Key: " + keyPair[1]);
+		System.out.println(SecretUtil.decryptDatasource("UFMmeZA9V0IoA4yVh/9hcRtvJMlAjSdJF9NGLiBgotnwcVr/Vgtbpnw/xjDnqFYh7QwW4ooQlwzSi7a7K7Mz3w=="));
 		System.out.println(SecretUtil.encryptDatasource("moiaP@12!"));
 
 	}
