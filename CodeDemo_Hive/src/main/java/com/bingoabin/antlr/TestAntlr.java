5dc0c7c534464bd5da4f9e09354695514bd40717
bingo abin <757661238@qq.com>
2023-10-11 12:03:05 +0800

    hive antlr

---
+++ CodeDemo_Hive/src/main/java/com/bingoabin/antlr/TestAntlr.java
@@ -0,0 +1,19 @@
+package com.bingoabin.antlr;
+
+import org.apache.hadoop.hive.ql.parse.ASTNode;
+import org.apache.hadoop.hive.ql.parse.ParseDriver;
+import org.apache.hadoop.hive.ql.parse.ParseException;
+
+/**
+ * @author bingoabin
+ * @date 2023/10/11 9:49
+ * @Description:
+ */
+public class TestAntlr {
+	public static void main(String[] args) throws ParseException {
+		ParseDriver parseDriver = new ParseDriver();
+		String sql = "create external table if not exists DL_SDB.EXT_MAIN (id STRING) stored as textfile location '/datalake/user/${hivevar:EXT_DATA}/date'";
+		ASTNode node = parseDriver.parse(sql);
+		System.out.println("success");
+	}
+}
