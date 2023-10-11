f266da8d92a90a92224f530458e658c537c9c811
bingo abin <757661238@qq.com>
2023-10-11 09:48:46 +0800

    technic

--- CodeDemo_Java/src/main/java/com/bingoabin/aatechnic/technic/Technic.java
+++ CodeDemo_Java/src/main/java/com/bingoabin/aatechnic/technic/Technic.java
@@ -10,11 +10,15 @@ import org.junit.Test;
 public class Technic {
 	@Test
 	public void test(){
+		//快捷键技巧
 		//1. shift shift 全局搜索  注意其中的git，可以搜索git提交的东西
 		//2. ctrl shift v 在idea中复制过的东西都在里面，可以一键粘贴
 		//3. ctrl E  平时看了哪些类
 		//4. alt 2   快速查看书签 定义书签右击ADD Bookmark
 
-
+		//源码阅读技巧
+		//1.ctrl alt B 查看接口实现 service.test()  进到test方法，通过ctrl 左键只是到接口，用ctrl alt B能直接到实现
+		//2.添加书签  还可以右击添加快捷键  全局ctrl + 2可以快速定位
+		//3.
 	}
 }
