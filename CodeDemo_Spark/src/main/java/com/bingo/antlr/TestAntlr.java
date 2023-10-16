0863d8e04cd5f8b252a15203f6f8876b959c2c29
bingo abin <757661238@qq.com>
2023-10-16 08:59:42 +0800

    common tools

---
+++ CodeDemo_Spark/src/main/java/com/bingo/antlr/TestAntlr.java
@@ -0,0 +1,15 @@
+package com.bingo.antlr;
+
+import org.apache.spark.sql.catalyst.parser.CatalystSqlParser;
+
+/**
+ * @author bingoabin
+ * @date 2023/10/16 8:55:05
+ * @Description:
+ */
+public class TestAntlr {
+	public static void main(String[] args){
+		CatalystSqlParser parser = new CatalystSqlParser(null);
+		String sql = "";
+	}
+}
