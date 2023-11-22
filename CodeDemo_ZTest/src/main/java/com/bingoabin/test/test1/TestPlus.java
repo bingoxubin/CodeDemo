c3fb76f0b4d0a3f90714114d81029192fa0fdba0
bingo abin <757661238@qq.com>
2023-11-22 08:02:05 +0800

    hanoitower

---
+++ CodeDemo_ZTest/src/main/java/com/bingoabin/test/test1/TestPlus.java
@@ -0,0 +1,12 @@
+package com.bingoabin.test.test1;
+
+/**
+ * @author bingoabin
+ * @date 2023/11/22 7:51:18
+ * @Description:
+ */
+public class TestPlus {
+	public static void main(String[] args){
+		System.out.println(0.1 + 0.2);
+	}
+}
